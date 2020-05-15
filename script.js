0fce14afe31b7cc7513d7d4ff4ba5c81d33805f4
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-05-14 20:51:20 -0400

    init commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// TODO: add code here
+ //30 lines or less challenge
+//const import json file 
+
+ document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+        response.json().then( function(json) {
+            const div = document.getElementById("container");
+            div.innerHTML = `
+                <ul>
+                    <li>ID #: ${json.id}</li>
+                    <li>Active: ${json.active}</li>
+                    <li>First Name: ${json.firstName}</li>
+                    <li>Last Name: ${json.lastName}</li>
+                    <li>Skills: ${json.skills}</li>
+                    <li>Hours In Space: ${json.hoursInSpace}</li>
+                    <img src="${json.picture}" />
+                </ul>
+            `;
+        });
+    });
+});
