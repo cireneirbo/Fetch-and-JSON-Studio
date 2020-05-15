a88e7702b0fd0bac818e53ac64f1fe2df7366235
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-05-14 21:18:48 -0400

    20 lines of magic

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
- //30 lines or less challenge
-//const import json file 
-
  document.addEventListener("DOMContentLoaded", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
         response.json().then( function(json) {
             const div = document.getElementById("container");
-            div.innerHTML = `
-                <ul>
-                    <li>ID #: ${json.id}</li>
-                    <li>Active: ${json.active}</li>
-                    <li>First Name: ${json.firstName}</li>
-                    <li>Last Name: ${json.lastName}</li>
-                    <li>Skills: ${json.skills}</li>
-                    <li>Hours In Space: ${json.hoursInSpace}</li>
-                    <img src="${json.picture}" />
-                </ul>
-            `;
+            for (let i = 0; i < json.length; i++) {
+                div.innerHTML += `
+                    <fieldset>
+                        <li>ID #: ${json[i].id}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>First Name: ${json[i].firstName}</li>
+                        <li>Last Name: ${json[i].lastName}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                        <li>Hours In Space: ${json[i].hoursInSpace}</li>
+                        <img class="images" src="${json[i].picture}" />
+                    </fieldset>
+                `;                
+            }            
         });
     });
 });
